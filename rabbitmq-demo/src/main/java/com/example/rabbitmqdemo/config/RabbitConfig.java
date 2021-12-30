17bf3a8ef2e8eed9848c228c189eddb812fdd3ab
youlinwei <1476219151@qq.com>
2021-12-30 13:54:31 +0800

    放一个HTML文件

---
+++ rabbitmq-demo/src/main/java/com/example/rabbitmqdemo/config/RabbitConfig.java
@@ -0,0 +1,9 @@
+package com.example.rabbitmqdemo.config;
+
+/**
+ * @author 爱吃西瓜的番茄酱
+ * @version 1.0
+ * @date 2021/8/24 14:26
+ */
+public class RabbitConfig {
+}
---
+++ rabbitmq-demo/src/main/java/com/example/rabbitmqdemo/consumer/RabbitConsumer.java
@@ -0,0 +1,9 @@
+package com.example.rabbitmqdemo.consumer;
+
+/**
+ * @author 爱吃西瓜的番茄酱
+ * @version 1.0
+ * @date 2021/8/24 14:39
+ */
+public class RabbitConsumer {
+}
